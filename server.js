5034b30d60f6c714a4701d5c289af725bd4393f6
Felipe <felipesa1411@.com>
2023-10-19 16:13:08 -0300

    feat

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 // Vamos criar um servidor local HTTP
+// import http from "http"
+import app from "./src/app.js"
 
-import http from "http"
 
 const PORT = 3000 // Porta padrão ultilizada
 
-const rotas = { 
-    "/":"Te Amo meu amooooooooooor !!!!!!", // Ao acessar a URL base, ele vai exibir o valor da chave
-    "/express":"Express ", // Ao acessar a URL base, ele vai exibir o valor da chave
-    "/mongoDB":"Mongo DB" // Ao acessar a URL base, ele vai exibir o valor da chave
-}
-
-const server = http.createServer((req,res)=>{
-    res.writeHead(200, {"Content-type":"text/plain"})
-    res.end(rotas[req.url])
-})
-
-server.listen(PORT, ()=> {
-    console.log("Voxe é meu mozinhoooooooo")
+app.listen(PORT, ()=> {
+    console.log("Tá na escuta pâe")
 
 })
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,40 @@
 import express from "express";
+
+const app  = express()
+app.use(express.json()) //Ajuda a trabalhar com dados JSON nas solicitações HTTP
+
+const livros = [
+    {
+        id:1,
+        titulo: "A Arte de Pensar Claramente"
+    },
+    {
+        id:2,
+        titulo:"Codigo Limpo"
+    }
+]
+
+function buscaLivro(id){
+    return livros.findIndex(livro=>{
+        return livro.id === Number(id)
+    })
+}
+app.get("/", (req,res)=>{
+    res.status(200).send("Aaaa zé da manga")
+})
+app.get("/livros", (req,res)=>{
+    res.status(200).json(livros)
+})
+
+app.get("/livros/:id", (req, res)=>{
+    const index = buscaLivro(req.params.id)
+    res.status(200).json(livros[index])
+
+})
+
+app.post("/livros", (req,res)=>{
+    livros.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso")
+})
+
+export default app
